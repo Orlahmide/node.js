a93206f4319bfa4cd9efb3489c71c6d3f2d69cc7
orlahmide <tomilolaaturaka@gmail.com>
2025-01-31 16:56:36 +0100

    assignment

---
+++ node.js
@@ -0,0 +1,61 @@
+// let numbers = [1, 2, 3, 4]
+
+// function sum([a, b, c, d]) {
+
+//     return a + b + c + d
+// }
+
+// console.log(sum(numbers));
+
+// function arrMethod(numberss){
+//     return numberss.reduce((a,b) => a+b , 0)
+// }
+
+// console.log(arrMethod(numbers))
+
+
+
+
+// function odd(arr){
+//     let number = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 == 1) {
+//             number = number + arr[i]
+//         }       
+//     }
+//     return number   
+// 
+
+// let arr = [1, 2, 3, 4, 5,6,9,7]
+// console.log(odd(arr))
+
+// let result = 0
+// arr.forEach(element => {
+   
+//    result = result + element
+   
+// });
+// console.log(result)
+
+
+
+// function r(t) {
+//     let sum = 0
+//     for(let i = 0; i < t.length; i++){
+//         if( t.le)
+//        sum = sum + t[i]
+//     }
+//     return sum
+  
+// }
+// let t = [2,5,6,7,8,9];
+// console.log(r(t));
+
+arr = [1,2,3,4]
+for (i = 1; i<arr.length ; i++){
+
+    console.log(arr[i])
+    return 
+}
+
+
