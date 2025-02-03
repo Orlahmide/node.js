f2aed2b656905577f8b735a9edfb4c6450190d3a
Orlahmide <tomilolaaturaka@gmail.com>
2025-02-03 01:33:09 +0100

    assignment

--- node.js
+++ node.js
@@ -51,11 +51,69 @@
 // let t = [2,5,6,7,8,9];
 // console.log(r(t));
 
-arr = [1,2,3,4]
-for (i = 1; i<arr.length ; i++){
+// arr = [1,2,3,4]
+// for (i = 1; i<arr.length ; i++){
 
-    console.log(arr[i])
-    return 
+//     console.log(arr[i])
+//     return 
+// }
+
+
+function printFactor(number){
+    if(number < 1){
+        console.log("invalid value")
+    }
+    let factor = []
+    for(let i = 0; i <= number; i++){
+        
+        if(number%i == 0){
+            factor.push(i)
+        }   
+    }
+    return factor
+}   
+
+console.log(printFactor(8))
+
+function sumFirstAndLastDigit(number){
+    let n = number.toString();
+    let firstDigit = parseInt(n[0]);
+    if(n.length==1){
+        return n[0] * 2
+    }
+    else if(n < 0){
+        return -1
+    }
+    let lastDigit = parseInt(n[n.length-1])
+    let sum = firstDigit + lastDigit;
+    return sum
 }
+
+console.log(sumFirstAndLastDigit(252))
+
+function evenDigit(number){
+
+    let arrN=number.toString()
+    let even = 0
+    for( let i = 0; i<arrN.length; i++ ){
+        if( arrN[i] % 2 == 0){
+            even = even + parseInt(arrN[i])
+        }
+    }
+    return even
+}
+
+console.log(evenDigit(123456789))
+
+function reverse(value){
+    let str = value.toString()
+    let t = ""
+    for( let i = str.length-1; i >= 0; i--){
+        t = t + str[i]
+    }
+    return t
+}
+console.log(reverse("Aturaka Tomilola"))
+
 
 
